2c52d4a42c4ef49d77de362e8bdbadf728c05304
Tushar Patel <tushar132812@gmail.com>
2021-10-02 13:08:06 +0530

    Edit Storages

--- PilotStreamingProject/storages.py
+++ PilotStreamingProject/storages.py
@@ -3,8 +3,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class MediaStorage(S3Boto3Storage):
-    location = settings.STATIC_LOCATION
+    location = settings.MEDIA_LOCATION
 
 
 class StaticStorage(S3Boto3Storage):
-    location = settings.MEDIA_LOCATION
+    location = settings.STATIC_LOCATION
