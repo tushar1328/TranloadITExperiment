b614b417abd3ea7025fbf3ab31b3d1b866e628df
Tushar Patel <tushar132812@gmail.com>
2021-10-03 02:01:27 +0530

    task updated

--- streaming/tasks.py
+++ streaming/tasks.py
@@ -19,7 +19,7 @@ def process_streaming_and_upload_to_s3(instance_id, file_url, ext,file_name):
         region_name=f'{settings.AWS_S3_REGION}'
     )
     streaming_file = ffmpeg_streaming.input(s3, bucket_name=f"{settings.AWS_STORAGE_BUCKET_NAME}", key=f"{(key.path)[1:]}")
-    output_url = f'/hlsdemooutput/{random.randint(111111111, 999999999)}/{file_name}'
+    output_url = f'hlsdemooutput/{random.randint(111111111, 999999999)}/{file_name}'
     save_to_s3 = CloudManager().add(s3, bucket_name=f"{settings.AWS_STORAGE_BUCKET_NAME}",folder=output_url,file_name="stream.m3u8")
     hls = streaming_file.hls(Formats.h264())
     if ext == ".mp3":
