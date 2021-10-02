f53cda53706bc55221bf38133aabfcd177612e1c
Tushar Patel <tushar132812@gmail.com>
2021-10-03 02:00:23 +0530

    task updated

--- streaming/tasks.py
+++ streaming/tasks.py
@@ -19,18 +19,16 @@ def process_streaming_and_upload_to_s3(instance_id, file_url, ext,file_name):
         region_name=f'{settings.AWS_S3_REGION}'
     )
     streaming_file = ffmpeg_streaming.input(s3, bucket_name=f"{settings.AWS_STORAGE_BUCKET_NAME}", key=f"{(key.path)[1:]}")
-    save_to_s3 = CloudManager().add(s3, bucket_name=f"{settings.AWS_STORAGE_BUCKET_NAME}")
+    output_url = f'/hlsdemooutput/{random.randint(111111111, 999999999)}/{file_name}'
+    save_to_s3 = CloudManager().add(s3, bucket_name=f"{settings.AWS_STORAGE_BUCKET_NAME}",folder=output_url,file_name="stream.m3u8")
     hls = streaming_file.hls(Formats.h264())
     if ext == ".mp3":
         _144p = Representation(Size(0, 0), Bitrate(95 * 1024, 64 * 1024))
         hls.representations(_144p)
-        st = 'audio'
     else:
         hls.auto_generate_representations()
-        st = 'video'
-    output_url = f'/hlsdemooutput/{random.randint(111111111, 999999999)}/{file_name}/{st}.m3u8'
-    hls.output(output_url, clouds=save_to_s3)
+    hls.output(clouds=save_to_s3)
     stream = Stream.objects.get(id=instance_id)
-    stream.streaming_url = f'https://{settings.AWS_S3_CUSTOM_DOMAIN}{output_url}'
+    stream.streaming_url = f'https://{settings.AWS_S3_CUSTOM_DOMAIN}{output_url}/stream.m3u8'
     stream.save()
 
