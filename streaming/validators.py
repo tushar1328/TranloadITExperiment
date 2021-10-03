6fe77c239ed9644c04871f4a1184cfd1266daf04
Tushar Patel <tushar132812@gmail.com>
2021-10-03 21:05:40 +0530

    Updated the app to final stages

--- streaming/validators.py
+++ streaming/validators.py
@@ -1,7 +1,15 @@
+import os
+
 from django.core.exceptions import ValidationError
 
 
-def file_size(value): # add this to some file where you can import it from
-    limit = 6 * 1024 * 1024
-    if value.size > limit:
-        raise ValidationError('File too large. Size should not exceed 5 MiB.')
+def file_size(value):
+    name, extension = os.path.splitext(value.name)
+    if extension == '.mp4':
+        limit = 18 * 1024 * 1024
+        if value.size > limit:
+            raise ValidationError('File too large. Size should not exceed 18 MiB.')
+    else:
+        limit = 6 * 1024 * 1024
+        if value.size > limit:
+            raise ValidationError('File too large. Size should not exceed 5 MiB.')
