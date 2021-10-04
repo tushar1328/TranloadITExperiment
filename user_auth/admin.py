29d633849656ede525693ba8c56d4bb66f79e906
Tushar Patel <tushar132812@gmail.com>
2021-10-04 12:22:19 +0530

    Updated Admin

--- user_auth/admin.py
+++ user_auth/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from streaming.models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form  = UserCreationForm
+    form = UserChangeForm
+    model = User
+
+
+admin.site.register(User,CustomUserAdmin)
